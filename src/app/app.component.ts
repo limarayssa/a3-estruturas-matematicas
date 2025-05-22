87d52fc2f8ff216f355a84993656b97157384d32
rayssa lima <dossantos.rayssalima@gmail.com>
2025-05-21 23:19:44 -0300

    Adicionando campo para seleção dos navios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { InitialPageComponent } from "./pages/initial-page/initial-page.component";
 import { HeaderComponent } from "./core/components/header/header.component";
 import { FooterComponent } from "./core/components/footer/footer.component";
+import { ChooseShipsComponent } from "./pages/choose-ships/choose-ships/choose-ships.component";
 
 @Component({
   selector: 'app-root',
-  imports: [InitialPageComponent, HeaderComponent, FooterComponent],
+  imports: [InitialPageComponent, HeaderComponent, FooterComponent, ChooseShipsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/choose-ships/choose-ships/choose-ships.component.ts
+++ src/app/pages/choose-ships/choose-ships/choose-ships.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-choose-ships',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './choose-ships.component.html',
   styleUrl: './choose-ships.component.css'
 })
 export class ChooseShipsComponent {
+
+  tabuleiro: string[][] = Array(10).fill(null).map(() => Array(10).fill('vazio'));
+  navio: string[] = [];
+  // colunas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+
+
 
 }
