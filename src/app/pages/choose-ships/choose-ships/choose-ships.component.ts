50868e6ff1c58cc9729d8e4f04658d627e28962f
rayssa lima <dossantos.rayssalima@gmail.com>
2025-05-22 23:20:03 -0300

    tabuleiro ccriado e seleção dos campos

--- src/app/pages/choose-ships/choose-ships/choose-ships.component.ts
+++ src/app/pages/choose-ships/choose-ships/choose-ships.component.ts
@@ -10,9 +10,12 @@ import { CommonModule } from '@angular/common';
 export class ChooseShipsComponent {
 
   tabuleiro: string[][] = Array(10).fill(null).map(() => Array(10).fill('vazio'));
+  colunas: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
   navio: string[] = [];
-  // colunas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+  maxNavios = 10;
 
-
+  onCellClick(x: number, y: number) {
+    alert(`Campo escolhido: ${this.colunas[y]}${x + 1}`);
+  }
 
 }
